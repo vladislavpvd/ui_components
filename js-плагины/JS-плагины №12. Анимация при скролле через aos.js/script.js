e17db7b08a020105b0cfc76fd5c9fbc556616565
Vladislav Pigachev <vladislav.pvd@gmail.com>
2022-06-12 12:26:21 +0400

    update

--- js-плагины/JS-плагины №12. Анимация при скролле через aos.js/script.js
+++
@@ -1,3 +0,0 @@
-AOS.init({
-	disable: 'phone'
-});
--- js-плагины/JS-плагины №14. Многострочное обрезание текста - Ellipsis.js/js/script.js
+++
@@ -1,9 +0,0 @@
-Ellipsis({
-  ellipsis: '…',
-  debounce: 300,
-  responsive: true,
-  className: '.text',
-  lines: 4,
-  portrait: null,
-  break_word: false
-});
